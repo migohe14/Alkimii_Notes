046d70c44788d87de70238eb443b9a9e8190fe66
migohe14 <migohe14@gmail.com>
2018-12-21 13:43:00 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   
-  resources :notes
-  root :to => "home#index"
-
   namespace :api do
     namespace :v1 do
       resources :notes do
       end
     end
   end
+
+  root :to => "home#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
